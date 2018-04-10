3e12634ba3eb5781275b80ddf1721bb674c0d516
pavka <pavel-mosein@yandex.ru>
2018-04-10 20:04:15 +0300

    Исправил коментарии

--- Parser.py
+++ Parser.py
@@ -3,7 +3,6 @@ import json
 def bsRequest(id):
     from bs4 import BeautifulSoup
     import requests
-    
     returnlist = {}
     print(id)
     r_comments = requests.get('http://forum.mfd.ru/forum/poster/comments/?id={id}'.format(id=id)).content
@@ -12,9 +11,9 @@ def bsRequest(id):
     soup_posts = BeautifulSoup(r_posts, "html.parser")
     comments = [p.text for p in soup_comments.find_all("div", {"class": "mfd-quote-text"})]
     posts = [p.text for p in soup_posts.find_all("div", {"class": "mfd-quote-text"})]
-    if len(comments) != 0:
-        returnlist["comment"] = comments[len(comments)-1]
-    if len(posts) != 0:
+    if len(comments) > 0:
+        returnlist["comment"] = comments[0]
+    if len(posts) > 0:
         returnlist["post"] = posts[len(posts)-1]
     print(returnlist)
     return returnlist
@@ -22,7 +21,6 @@ def bsRequest(id):
 def newMessageCheck(bot, update):
     import database
     from settings import my_id
-    
     with open('data.json') as json_input:
         data = json.load(json_input)
         json_input.close()
@@ -36,7 +34,7 @@ def newMessageCheck(bot, update):
         if "comment" in bsdata:
             if data[j]["comment_hex"] != database.makeMD5(bsdata["comment"]):
                 bot.sendMessage(chat_id=my_id,
-                                text='New comment from {id}:{post}'.format(id=j, post=bsdata["post"]))
+                                text='New comment from {id}:{comment}'.format(id=j, post=bsdata["comment"]))
                 data[j]["comment_hex"] = database.makeMD5(bsdata["comment"])
     with open('data.json','w') as json_export:
         json.dump(data, json_export)
