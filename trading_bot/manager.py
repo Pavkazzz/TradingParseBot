fd7a3479ea8c8a2bf72b15bb9251c473d605e214
pavkazzz <pavel-mosein@yandex.ru>
2018-09-08 15:15:46 +0300

    error with set

--- trading_bot/manager.py
+++ trading_bot/manager.py
@@ -278,7 +278,7 @@ class Manager:
             user_msg = frozenset(self.sended_msg[user].keys())
             actual_set = user_msg - frozenset(actual_id_list)
             if len(actual_set) > 0:
-                self.sended_msg[user].pop(actual_set)
+                self.sended_msg[user].pop(list(actual_set))
 
 
         return self.sended_msg
